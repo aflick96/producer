30484c698669520c59f9e2617444073918ded60a
Andrew Flick <andrew.flick@jhuapl.edu>
2024-09-04 13:29:07 -0400

    altered process

--- src/process.py
+++ src/process.py
@@ -12,7 +12,12 @@ def generateRandomString(length = 10):
 def generateDataSet():
     #define out directory
     current_directory = os.path.dirname(__file__)
+
+    print(current_directory)
+
     output_directory = os.path.join(current_directory, '../output')
+
+    print(output_directory)
 
     # create output directory if it does not exist
     if not os.path.exists(output_directory):
@@ -20,15 +25,16 @@ def generateDataSet():
 
     # generate new data set and timestamp of completion
     dataSet = [generateRandomString() for _ in range(10000)]
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
-    # output to csv
-    df = pd.DataFrame(dataSet, columns=['data'])
-    df.to_csv(os.path.join(output_directory, f'{timestamp}.csv'), index=False)
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     # output timestamp to txt
     with open(os.path.join(output_directory, 'timestamp.txt'), 'w') as f:
         f.write(timestamp)
+
+    # output to csv
+    df = pd.DataFrame(dataSet, columns=['data'])
+    df.to_csv(os.path.join(output_directory, 'db_data.csv'), index=False)
+
 
 if __name__ == '__main__':
     generateDataSet()
